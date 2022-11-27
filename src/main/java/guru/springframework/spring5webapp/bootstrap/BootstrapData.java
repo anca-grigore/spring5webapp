57485281d1085432ae027232b4f6647a3d4696e8
Anca Grigore <anca.grigore@realpage.com>
2022-11-27 15:22:58 +0200

    Assignment 2: Create Publisher Entity

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,11 +14,13 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository,
+                         PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -40,5 +44,16 @@ public class BootstrapData implements CommandLineRunner {
         System.out.println("Started in Bootstrap");
         System.out.println("Number of books: " + bookRepository.count());
 
+        Publisher nemira = new Publisher("Nemira", "Iani Buzoiani 14", "Bucharest",
+                "Romania", "109321");
+
+        Book friends = new Book("Friends, iubiri si marele lucru teribil", "210948294");
+        Author matthew = new Author("Matthew", "Perry");
+        eric.getBooks().add(friends);
+        friends.getAuthors().add(matthew);
+
+        publisherRepository.save(nemira);
+
+        System.out.println("Publisher repository: " + publisherRepository.findAll());
     }
 }
