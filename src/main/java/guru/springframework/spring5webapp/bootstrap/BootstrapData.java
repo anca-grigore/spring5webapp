1ae7f681924df2926c13d625a971106139afcc8b
Anca Grigore <anca.grigore@realpage.com>
2022-11-27 16:50:29 +0200

    Adding thymeleaf

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -32,6 +32,8 @@ public class BootstrapData implements CommandLineRunner {
         publisher.setCity("St. Petersburg");
         publisher.setState("FL");
 
+        publisherRepository.save(publisher);
+
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "123123");
         eric.getBooks().add(ddd);
